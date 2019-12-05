ae65087ab9888b4d62468e867db972f369c38e89
maninderjits <sukhisembhy@hotmail.com>
2019-12-05 15:02:43 -0800

    commented out loop added socket programming using bluetooth

--- rasp_pi_andr_bluetooth_radio.py
+++ rasp_pi_andr_bluetooth_radio.py
@@ -1,9 +1,11 @@
 ###This file curently only takes care of sending side, meaning this code 
 ###	only reads data which a user wants to send, the receiver side still
 ###	needs to be added.
+#B8:27:EB:0A:26:6F
 
 import os
 import time
+import bluetooth
 from digi.xbee.devices import XBeeDevice
 
 ##Make an instance of XBeeDevice and opening a connection with the device
@@ -13,23 +15,50 @@ try:
 except Exception as e:
 	print(str(e))
 
+##Bluetooth####
+hostMACAddress = "B8:27:EB:0A:26:6F" #for bluetooth interface
+port = 0
+backlog = 1
+size = 1024
+s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+
+try:
+	s.bind((hostMACAddress, port))
+	s.listen(backlog)
+except Exception as e:
+	print(str(e))
+
 ##File where the android device will be writing to 
 exists = os.path.isfile('/dev/rfcomm0') 
 
+##
+try:
+	client, clientInfo = s.accept()
+	while 1:
+		data = client.recv(size)
+		if data:
+			print(data)
+			client.send(data) # Echo back to client
+except Exception as e:	
+    print("Closing socket: " + e)
+    client.close()
+    s.close()
+
+
 ##This infinite loop is checking from new data received from the user 
 ##	(android device) every 5 seconds
-while(1):
-	time.sleep(5)
-	if exists:
-		file = open("/dev/rfcomm0", "r")
-		data_read = file.read()
-		if not r:
-			print("File is empty, no data to retrieve.")
-		else:
-			send_message(mssg)
-			print(data_read)
-	else:
-		print("The file doesn't exist. \n\tCheck bluetooth connection with phone.")
+#~ while(1):
+	#~ time.sleep(5)
+	#~ if exists:
+		#~ file = open("/dev/rfcomm0", "r")
+		#~ data_read = file.read()
+		#~ if not r:
+			#~ print("File is empty, no data to retrieve.")
+		#~ else:
+			#~ send_message(mssg)
+			#~ print(data_read)
+	#~ else:
+		#~ print("The file doesn't exist. \n\tCheck bluetooth connection with phone.")
 
 ##Closing the connection
 device.close()
